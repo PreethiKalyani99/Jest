ce9f561242d5cab1c4e98ef57005af9f1588895b
Preethi-Kalyani <preethikalyani99@gmail.com>
2024-02-09 10:34:45 +0530

    adding little lemon js file

---
+++ littleLemon.js
@@ -0,0 +1,19 @@
+function getPrices(includeTax, dishData){
+    const taxRate = 0.2
+    let str = ''
+    str += includeTax ? "Prices with 20% tax:" : "Prices without tax:"
+    const result = dishData.reduce((acc,cur) => {
+        if(includeTax){
+            const taxAmount = cur.price * taxRate
+            const finalPrice = cur.price + taxAmount 
+            acc += `\nDish: ${cur.name} Price (incl.tax): $ ${finalPrice.toFixed(2)}`
+        }
+        else{
+            acc += `\nDish: ${cur.name} Price: $ ${cur.price}`
+        }
+        return acc
+    },'')
+    str += result
+    return str
+}
+module.exports = getPrices
